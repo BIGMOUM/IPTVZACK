f6a9479078473a9126860a5bdda8e35fd6901765
BIGMOUM <105049126+BIGMOUM@users.noreply.github.com>
2023-10-04 10:13:05 +0100

    Add files via upload

---
+++ Shared/ipc-commands.ts
@@ -0,0 +1,25 @@
+// EPG related commands
+export const EPG_FETCH = 'EPG:FETCH';
+export const EPG_FETCH_DONE = 'EPG:FETCH_DONE';
+export const EPG_ERROR = 'EPG:ERROR';
+export const EPG_GET_PROGRAM = 'EPG:GET_PROGRAM';
+export const EPG_GET_PROGRAM_DONE = 'EPG:GET_PROGRAM_DONE';
+
+// Playlist related commands
+export const PLAYLIST_SAVE_DETAILS = 'PLAYLIST:SAVE_DETAILS';
+export const PLAYLIST_PARSE = 'PLAYLIST:PARSE_PLAYLIST';
+export const PLAYLIST_PARSE_RESPONSE = 'PLAYLIST:PARSE_PLAYLIST_RESPONSE';
+export const PLAYLIST_UPDATE = 'PLAYLIST:UPDATE';
+export const PLAYLIST_UPDATE_RESPONSE = 'PLAYLIST:UPDATE_RESPONSE';
+export const PLAYLIST_UPDATE_POSITIONS = 'PLAYLIST:UPDATE_POSITIONS';
+
+// General
+export const SHOW_WHATS_NEW = 'SHOW_WHATS_NEW';
+export const ERROR = 'error';
+
+// Channels
+export const CHANNEL_SET_USER_AGENT = 'CHANNEL:SET_USER_AGENT';
+
+// Views
+export const VIEW_SETTINGS = 'settings-view';
+export const VIEW_ADD_PLAYLIST = 'add-playlist-view';
